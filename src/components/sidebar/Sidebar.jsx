1aced4ca9e213aefcda2ed3f5ea461ad6baaa41e
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-01-29 10:53:38 +0530

    retouch sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,27 +6,21 @@ const Sidebar = () => {
       <aside className={style.aside1}>
         <div className={style.link} data-link="dashboard">
           <div className={style.icon}>
-            <span className="material-symbols-outlined">download</span>
+            <span className="material-symbols-outlined">description</span>
           </div>
-          <div className={style.text}>download</div>
+          <div className={style.text}>User Manual</div>
+        </div>
+        <div className={style.link} data-link="dashboard">
+          <div className={style.icon}>
+            <span className="material-symbols-outlined">description</span>
+          </div>
+          <div className={style.text}>Sale Conditions</div>
         </div>
         <div className={style.link} data-link="dashboard">
           <div className={style.icon}>
             <span className="material-symbols-outlined">download</span>
           </div>
-          <div className={style.text}>download</div>
-        </div>
-        <div className={style.link} data-link="dashboard">
-          <div className={style.icon}>
-            <span className="material-symbols-outlined">download</span>
-          </div>
-          <div className={style.text}>download</div>
-        </div>
-        <div className={style.link} data-link="dashboard">
-          <div className={style.icon}>
-            <span className="material-symbols-outlined">download</span>
-          </div>
-          <div className={style.text}>download</div>
+          <div className={style.text}>AnyDesk</div>
         </div>
         <div className={style.link} data-link="dashboard">
           <div className={style.icon}>
